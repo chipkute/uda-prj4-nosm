e5e574111f07085dbb3dda56226d79bc55641212
nosm <nosmdev1999@gmail.com>
2024-10-13 23:28:53 +0700

    update war to jar

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-SSH_KEY="/tmp/nosm-ec2-key.pem"
+echo 'The following Maven command installs your Maven-built Java application'
+echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
+set -x
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+set +x
 
-EC2_USER="ec2-user"
-EC2_IP="ec2-18-234-94-74.compute-1.amazonaws.com"
+echo 'The following command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+set -x
+NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+set +x
 
-REMOTE_DIR="/home/ec2-user"       # Thư mục trên EC2
-WAR_FILE="auth-course-0.0.1-SNAPSHOT.war"  # Tên file WAR
-LOCAL_WAR_PATH="target/$WAR_FILE" # Đường dẫn đến file WAR trong thư mục target
-REMOTE_WAR="$REMOTE_DIR/$WAR_FILE" # Đường dẫn đến file WAR trên EC2
+echo 'The following command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+set -x
+VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+set +x
 
-# Bước 1: Dừng ứng dụng nếu đang chạy
-echo "Stopping existing application..."
-ssh -i $SSH_KEY $EC2_USER@$EC2_IP "pkill -f 'java -jar $REMOTE_WAR'"
-
-# Bước 2: Copy file WAR từ Jenkins đến EC2
-echo "Copying WAR file to EC2..."
-scp -i $SSH_KEY $LOCAL_WAR_PATH $EC2_USER@$EC2_IP:$REMOTE_DIR/
-
-# Bước 3: Chạy ứng dụng Spring Boot mới
-echo "Starting the application..."
-ssh -i $SSH_KEY $EC2_USER@$EC2_IP "nohup java -jar $REMOTE_WAR > /dev/null 2>&1 &"
-
-echo "Deployment completed successfully!"
-
-# Bước 2: SSH vào EC2, dừng ứng dụng cũ và khởi động lại ứng dụng
-echo "Deploying application on EC2..."
-ssh -i $SSH_KEY $EC2_USER@$EC2_IP << EOF
-    echo "Stopping existing application (if any)..."
-    pkill -f 'java -jar' || true
-    echo "Starting new application..."
-    nohup java -jar $REMOTE_JAR > app.log 2>&1 &
-    echo "Application deployed!"
-EOF
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+set -x
+java -jar target/${NAME}-${VERSION}.jar
